bb7c52eadb83664f145a1d3cabc4aa5c99951e1c
Jakub Kulík <kulikjak@gmail.com>
2017-11-20 09:43:38 +0100

    Fixed Dijkstra algorithm with OpenACC support

--- dijkstra.c
+++ dijkstra.c
@@ -2,63 +2,58 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-//pgcc -acc -Minfo=acc -O2 dijkstra.c -o dijkstra
+// pgcc -acc -Minfo=acc -O2 dijkstra.c -o dijkstra
 #include "utils.h"
 
 double _start_in, _start_out;
 double _end_in, _end_out;
 
 matrix dijkstra_all(matrix distance_matrix, int32_t size) {
+  int32_t from;
+
   #pragma acc data copy(distance_matrix[0 : size][0 : size])
   {
-    _start_in = omp_get_wtime();  // clock();
-    #pragma acc parallel num_gangs(1024) vector_length(128)
-    #pragma acc loop independent
-    int32_t from;
-    for (from = 0; from < size; from++) {
-      int32_t *distance = (int32_t*) malloc (size * sizeof(int32_t));
-      int32_t *visited = (int32_t*) malloc (size * sizeof(int32_t));
-      int32_t i, count, mindistance, nextnode;;
-
-      #pragma acc loop
-      for(i = 0; i < size; i++) {
-        distance[i] = distance_matrix[from][i];
-        visited[i] = 0;
-      }
-
-      distance[from] = 0;
-      visited[from] = 1;
-
-      #pragma acc loop
-      for(count = 1; count < size-1; count++) {
-        mindistance=INF;
+    _start_in = omp_get_wtime();
+    #pragma acc parallel
+    {
+      #pragma acc loop independent
+      for (from = 0; from < size; from++) {
+        int32_t *visited = (int32_t *)malloc(size * sizeof(int32_t));
+        int32_t i, count, mindistance, nextnode;
 
         #pragma acc loop
-        for(i = 0; i < size; i++) {
-          if(distance[i] < mindistance && !visited[i]) {
-            mindistance = distance[i];
-            nextnode = i;
+        for (i = 0; i < size; visited[i++] = 0) {}
+
+        visited[from] = 1;
+
+        #pragma acc loop
+        for (count = 1; count < size - 1; count++) {
+          mindistance = INF;
+
+          #pragma acc loop
+          for (i = 0; i < size; i++) {
+            if (distance_matrix[from][i] < mindistance && !visited[i]) {
+              mindistance = distance_matrix[from][i];
+              nextnode = i;
+            }
           }
-        }
 
-        visited[nextnode] = 1;
+          visited[nextnode] = 1;
 
-        #pragma acc loop
-        for(i = 0; i < size; i++) {
-          if(!visited[i]) {
-            if(mindistance + distance_matrix[nextnode][i] < distance[i]) {
-              #pragma acc atomic write
-              distance[i] = mindistance + distance_matrix[nextnode][i];
+          #pragma acc loop
+          for (i = 0; i < size; i++) {
+            if (!visited[i]) {
+              if (mindistance + distance_matrix[nextnode][i] < distance_matrix[from][i]) {
+                #pragma acc atomic write
+                distance_matrix[from][i] = mindistance + distance_matrix[nextnode][i];
+              }
             }
           }
         }
+        free(visited);
       }
-      free(visited);
-      free(distance_matrix[from]);
-
-      distance_matrix[from] = distance;
     }
-      _end_in = omp_get_wtime();  // clock();
+    _end_in = omp_get_wtime();
   }
   return distance_matrix;
 }
@@ -93,7 +88,7 @@ int main(int argc, char *argv[]) {
   distance_matrix = dijkstra_all(distance_matrix, size);
   _end_out = omp_get_wtime();  // clock();
 
-  print_matrix(distance_matrix, size);
+  // print_matrix(distance_matrix, size);
 
   free_matrix(distance_matrix, size);
   free_matrix(graph_matrix, size);
