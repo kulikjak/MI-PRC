af52268cf3c23b9addb382822b1983602c21618c
Jakub Kulík <kulikjak@gmail.com>
2017-12-19 23:41:06 +0100

    Fw measurements

---
+++ gpu_run.sh
@@ -0,0 +1,319 @@
+#!/bin/bash
+
+echo "100"
+echo "100" >> cuda_fw_0.out
+./cuda_fw_0 graph100.txt >> cuda_fw_0.out
+./cuda_fw_0 graph100.txt >> cuda_fw_0.out
+./cuda_fw_0 graph100.txt >> cuda_fw_0.out
+
+echo "200"
+echo "200" >> cuda_fw_0.out
+./cuda_fw_0 graph200.txt >> cuda_fw_0.out
+./cuda_fw_0 graph200.txt >> cuda_fw_0.out
+./cuda_fw_0 graph200.txt >> cuda_fw_0.out
+
+echo "300"
+echo "300" >> cuda_fw_0.out
+./cuda_fw_0 graph300.txt >> cuda_fw_0.out
+./cuda_fw_0 graph300.txt >> cuda_fw_0.out
+./cuda_fw_0 graph300.txt >> cuda_fw_0.out
+
+echo "400"
+echo "400" >> cuda_fw_0.out
+./cuda_fw_0 graph400.txt >> cuda_fw_0.out
+./cuda_fw_0 graph400.txt >> cuda_fw_0.out
+./cuda_fw_0 graph400.txt >> cuda_fw_0.out
+
+echo "500"
+echo "500" >> cuda_fw_0.out
+./cuda_fw_0 graph500.txt >> cuda_fw_0.out
+./cuda_fw_0 graph500.txt >> cuda_fw_0.out
+./cuda_fw_0 graph500.txt >> cuda_fw_0.out
+
+echo "600"
+echo "600" >> cuda_fw_0.out
+./cuda_fw_0 graph600.txt >> cuda_fw_0.out
+./cuda_fw_0 graph600.txt >> cuda_fw_0.out
+./cuda_fw_0 graph600.txt >> cuda_fw_0.out
+
+echo "700"
+echo "700" >> cuda_fw_0.out
+./cuda_fw_0 graph700.txt >> cuda_fw_0.out
+./cuda_fw_0 graph700.txt >> cuda_fw_0.out
+./cuda_fw_0 graph700.txt >> cuda_fw_0.out
+
+echo "800"
+echo "800" >> cuda_fw_0.out
+./cuda_fw_0 graph800.txt >> cuda_fw_0.out
+./cuda_fw_0 graph800.txt >> cuda_fw_0.out
+./cuda_fw_0 graph800.txt >> cuda_fw_0.out
+
+echo "900"
+echo "900" >> cuda_fw_0.out
+./cuda_fw_0 graph900.txt >> cuda_fw_0.out
+./cuda_fw_0 graph900.txt >> cuda_fw_0.out
+./cuda_fw_0 graph900.txt >> cuda_fw_0.out
+
+echo "1000"
+echo "1000" >> cuda_fw_0.out
+./cuda_fw_0 graph1000.txt >> cuda_fw_0.out
+./cuda_fw_0 graph1000.txt >> cuda_fw_0.out
+./cuda_fw_0 graph1000.txt >> cuda_fw_0.out
+
+echo "1500"
+echo "1500" >> cuda_fw_0.out
+./cuda_fw_0 graph1500.txt >> cuda_fw_0.out
+./cuda_fw_0 graph1500.txt >> cuda_fw_0.out
+./cuda_fw_0 graph1500.txt >> cuda_fw_0.out
+
+echo "2000"
+echo "2000" >> cuda_fw_0.out
+./cuda_fw_0 graph2000.txt >> cuda_fw_0.out
+./cuda_fw_0 graph2000.txt >> cuda_fw_0.out
+./cuda_fw_0 graph2000.txt >> cuda_fw_0.out
+
+echo "2500"
+echo "2500" >> cuda_fw_0.out
+./cuda_fw_0 graph2500.txt >> cuda_fw_0.out
+./cuda_fw_0 graph2500.txt >> cuda_fw_0.out
+./cuda_fw_0 graph2500.txt >> cuda_fw_0.out
+
+###################################
+
+echo "100"
+echo "100" >> cuda_fw_1.out
+./cuda_fw_1 graph100.txt >> cuda_fw_1.out
+./cuda_fw_1 graph100.txt >> cuda_fw_1.out
+./cuda_fw_1 graph100.txt >> cuda_fw_1.out
+
+echo "200"
+echo "200" >> cuda_fw_1.out
+./cuda_fw_1 graph200.txt >> cuda_fw_1.out
+./cuda_fw_1 graph200.txt >> cuda_fw_1.out
+./cuda_fw_1 graph200.txt >> cuda_fw_1.out
+
+echo "300"
+echo "300" >> cuda_fw_1.out
+./cuda_fw_1 graph300.txt >> cuda_fw_1.out
+./cuda_fw_1 graph300.txt >> cuda_fw_1.out
+./cuda_fw_1 graph300.txt >> cuda_fw_1.out
+
+echo "400"
+echo "400" >> cuda_fw_1.out
+./cuda_fw_1 graph400.txt >> cuda_fw_1.out
+./cuda_fw_1 graph400.txt >> cuda_fw_1.out
+./cuda_fw_1 graph400.txt >> cuda_fw_1.out
+
+echo "500"
+echo "500" >> cuda_fw_1.out
+./cuda_fw_1 graph500.txt >> cuda_fw_1.out
+./cuda_fw_1 graph500.txt >> cuda_fw_1.out
+./cuda_fw_1 graph500.txt >> cuda_fw_1.out
+
+echo "600"
+echo "600" >> cuda_fw_1.out
+./cuda_fw_1 graph600.txt >> cuda_fw_1.out
+./cuda_fw_1 graph600.txt >> cuda_fw_1.out
+./cuda_fw_1 graph600.txt >> cuda_fw_1.out
+
+echo "700"
+echo "700" >> cuda_fw_1.out
+./cuda_fw_1 graph700.txt >> cuda_fw_1.out
+./cuda_fw_1 graph700.txt >> cuda_fw_1.out
+./cuda_fw_1 graph700.txt >> cuda_fw_1.out
+
+echo "800"
+echo "800" >> cuda_fw_1.out
+./cuda_fw_1 graph800.txt >> cuda_fw_1.out
+./cuda_fw_1 graph800.txt >> cuda_fw_1.out
+./cuda_fw_1 graph800.txt >> cuda_fw_1.out
+
+echo "900"
+echo "900" >> cuda_fw_1.out
+./cuda_fw_1 graph900.txt >> cuda_fw_1.out
+./cuda_fw_1 graph900.txt >> cuda_fw_1.out
+./cuda_fw_1 graph900.txt >> cuda_fw_1.out
+
+echo "1000"
+echo "1000" >> cuda_fw_1.out
+./cuda_fw_1 graph1000.txt >> cuda_fw_1.out
+./cuda_fw_1 graph1000.txt >> cuda_fw_1.out
+./cuda_fw_1 graph1000.txt >> cuda_fw_1.out
+
+echo "1500"
+echo "1500" >> cuda_fw_1.out
+./cuda_fw_1 graph1500.txt >> cuda_fw_1.out
+./cuda_fw_1 graph1500.txt >> cuda_fw_1.out
+./cuda_fw_1 graph1500.txt >> cuda_fw_1.out
+
+echo "2000"
+echo "2000" >> cuda_fw_1.out
+./cuda_fw_1 graph2000.txt >> cuda_fw_1.out
+./cuda_fw_1 graph2000.txt >> cuda_fw_1.out
+./cuda_fw_1 graph2000.txt >> cuda_fw_1.out
+
+echo "2500"
+echo "2500" >> cuda_fw_1.out
+./cuda_fw_1 graph2500.txt >> cuda_fw_1.out
+./cuda_fw_1 graph2500.txt >> cuda_fw_1.out
+./cuda_fw_1 graph2500.txt >> cuda_fw_1.out
+
+###################################
+
+echo "100"
+echo "100" >> cuda_fw_2.out
+./cuda_fw_2 graph100.txt >> cuda_fw_2.out
+./cuda_fw_2 graph100.txt >> cuda_fw_2.out
+./cuda_fw_2 graph100.txt >> cuda_fw_2.out
+
+echo "200"
+echo "200" >> cuda_fw_2.out
+./cuda_fw_2 graph200.txt >> cuda_fw_2.out
+./cuda_fw_2 graph200.txt >> cuda_fw_2.out
+./cuda_fw_2 graph200.txt >> cuda_fw_2.out
+
+echo "300"
+echo "300" >> cuda_fw_2.out
+./cuda_fw_2 graph300.txt >> cuda_fw_2.out
+./cuda_fw_2 graph300.txt >> cuda_fw_2.out
+./cuda_fw_2 graph300.txt >> cuda_fw_2.out
+
+echo "400"
+echo "400" >> cuda_fw_2.out
+./cuda_fw_2 graph400.txt >> cuda_fw_2.out
+./cuda_fw_2 graph400.txt >> cuda_fw_2.out
+./cuda_fw_2 graph400.txt >> cuda_fw_2.out
+
+echo "500"
+echo "500" >> cuda_fw_2.out
+./cuda_fw_2 graph500.txt >> cuda_fw_2.out
+./cuda_fw_2 graph500.txt >> cuda_fw_2.out
+./cuda_fw_2 graph500.txt >> cuda_fw_2.out
+
+echo "600"
+echo "600" >> cuda_fw_2.out
+./cuda_fw_2 graph600.txt >> cuda_fw_2.out
+./cuda_fw_2 graph600.txt >> cuda_fw_2.out
+./cuda_fw_2 graph600.txt >> cuda_fw_2.out
+
+echo "700"
+echo "700" >> cuda_fw_2.out
+./cuda_fw_2 graph700.txt >> cuda_fw_2.out
+./cuda_fw_2 graph700.txt >> cuda_fw_2.out
+./cuda_fw_2 graph700.txt >> cuda_fw_2.out
+
+echo "800"
+echo "800" >> cuda_fw_2.out
+./cuda_fw_2 graph800.txt >> cuda_fw_2.out
+./cuda_fw_2 graph800.txt >> cuda_fw_2.out
+./cuda_fw_2 graph800.txt >> cuda_fw_2.out
+
+echo "900"
+echo "900" >> cuda_fw_2.out
+./cuda_fw_2 graph900.txt >> cuda_fw_2.out
+./cuda_fw_2 graph900.txt >> cuda_fw_2.out
+./cuda_fw_2 graph900.txt >> cuda_fw_2.out
+
+echo "1000"
+echo "1000" >> cuda_fw_2.out
+./cuda_fw_2 graph1000.txt >> cuda_fw_2.out
+./cuda_fw_2 graph1000.txt >> cuda_fw_2.out
+./cuda_fw_2 graph1000.txt >> cuda_fw_2.out
+
+echo "1500"
+echo "1500" >> cuda_fw_2.out
+./cuda_fw_2 graph1500.txt >> cuda_fw_2.out
+./cuda_fw_2 graph1500.txt >> cuda_fw_2.out
+./cuda_fw_2 graph1500.txt >> cuda_fw_2.out
+
+echo "2000"
+echo "2000" >> cuda_fw_2.out
+./cuda_fw_2 graph2000.txt >> cuda_fw_2.out
+./cuda_fw_2 graph2000.txt >> cuda_fw_2.out
+./cuda_fw_2 graph2000.txt >> cuda_fw_2.out
+
+echo "2500"
+echo "2500" >> cuda_fw_2.out
+./cuda_fw_2 graph2500.txt >> cuda_fw_2.out
+./cuda_fw_2 graph2500.txt >> cuda_fw_2.out
+./cuda_fw_2 graph2500.txt >> cuda_fw_2.out
+
+###################################
+
+echo "100"
+echo "100" >> cuda_fw_3.out
+./cuda_fw_3 graph100.txt >> cuda_fw_3.out
+./cuda_fw_3 graph100.txt >> cuda_fw_3.out
+./cuda_fw_3 graph100.txt >> cuda_fw_3.out
+
+echo "200"
+echo "200" >> cuda_fw_3.out
+./cuda_fw_3 graph200.txt >> cuda_fw_3.out
+./cuda_fw_3 graph200.txt >> cuda_fw_3.out
+./cuda_fw_3 graph200.txt >> cuda_fw_3.out
+
+echo "300"
+echo "300" >> cuda_fw_3.out
+./cuda_fw_3 graph300.txt >> cuda_fw_3.out
+./cuda_fw_3 graph300.txt >> cuda_fw_3.out
+./cuda_fw_3 graph300.txt >> cuda_fw_3.out
+
+echo "400"
+echo "400" >> cuda_fw_3.out
+./cuda_fw_3 graph400.txt >> cuda_fw_3.out
+./cuda_fw_3 graph400.txt >> cuda_fw_3.out
+./cuda_fw_3 graph400.txt >> cuda_fw_3.out
+
+echo "500"
+echo "500" >> cuda_fw_3.out
+./cuda_fw_3 graph500.txt >> cuda_fw_3.out
+./cuda_fw_3 graph500.txt >> cuda_fw_3.out
+./cuda_fw_3 graph500.txt >> cuda_fw_3.out
+
+echo "600"
+echo "600" >> cuda_fw_3.out
+./cuda_fw_3 graph600.txt >> cuda_fw_3.out
+./cuda_fw_3 graph600.txt >> cuda_fw_3.out
+./cuda_fw_3 graph600.txt >> cuda_fw_3.out
+
+echo "700"
+echo "700" >> cuda_fw_3.out
+./cuda_fw_3 graph700.txt >> cuda_fw_3.out
+./cuda_fw_3 graph700.txt >> cuda_fw_3.out
+./cuda_fw_3 graph700.txt >> cuda_fw_3.out
+
+echo "800"
+echo "800" >> cuda_fw_3.out
+./cuda_fw_3 graph800.txt >> cuda_fw_3.out
+./cuda_fw_3 graph800.txt >> cuda_fw_3.out
+./cuda_fw_3 graph800.txt >> cuda_fw_3.out
+
+echo "900"
+echo "900" >> cuda_fw_3.out
+./cuda_fw_3 graph900.txt >> cuda_fw_3.out
+./cuda_fw_3 graph900.txt >> cuda_fw_3.out
+./cuda_fw_3 graph900.txt >> cuda_fw_3.out
+
+echo "1000"
+echo "1000" >> cuda_fw_3.out
+./cuda_fw_3 graph1000.txt >> cuda_fw_3.out
+./cuda_fw_3 graph1000.txt >> cuda_fw_3.out
+./cuda_fw_3 graph1000.txt >> cuda_fw_3.out
+
+echo "1500"
+echo "1500" >> cuda_fw_3.out
+./cuda_fw_3 graph1500.txt >> cuda_fw_3.out
+./cuda_fw_3 graph1500.txt >> cuda_fw_3.out
+./cuda_fw_3 graph1500.txt >> cuda_fw_3.out
+
+echo "2000"
+echo "2000" >> cuda_fw_3.out
+./cuda_fw_3 graph2000.txt >> cuda_fw_3.out
+./cuda_fw_3 graph2000.txt >> cuda_fw_3.out
+./cuda_fw_3 graph2000.txt >> cuda_fw_3.out
+
+echo "2500"
+echo "2500" >> cuda_fw_3.out
+./cuda_fw_3 graph2500.txt >> cuda_fw_3.out
+./cuda_fw_3 graph2500.txt >> cuda_fw_3.out
+./cuda_fw_3 graph2500.txt >> cuda_fw_3.out
