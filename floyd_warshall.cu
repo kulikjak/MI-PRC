03ab50ac39da945c002ecf16f061d5ca10ae8300
Jakub Kulík <kulikjak@gmail.com>
2017-12-19 18:16:43 +0100

    Added new better kernels

--- floyd_warshall.cu
+++ floyd_warshall.cu
@@ -4,8 +4,9 @@
 
 #include "cuda_utils.h"
 
-//#define _CHECK_MATRICES
-//#deinfe _SHARED
+// #define _CHECK_MATRICES
+// #define _PRINT_RESULT
+#define _KERNEL 0
 
 #define TILE_SIZE 32
 #define LOG_TILE_SIZE 5
@@ -68,12 +69,12 @@ __global__ void kernel_column_dependent_blocks(matrix __dm, int32_t __size,
                                                       : __dm[i][j];
 }
 
-// Kernel for double depenent blocks
+// Kernel for double depenent blocks (very simple)
 __global__ void kernel_double_dependent_blocks(matrix __dm, int32_t __size,
                                                int32_t __b, int32_t k) {
   // get i, j, ib and jb from block and thread information
-  const int32_t ib = blockIdx.y;
-  const int32_t jb = blockIdx.x;
+  const int32_t ib = blockIdx.x;
+  const int32_t jb = blockIdx.y;
 
   const int32_t i = threadIdx.x + (jb << LOG_TILE_SIZE);
   const int32_t j = threadIdx.y + (ib << LOG_TILE_SIZE);
@@ -89,10 +90,129 @@ __global__ void kernel_double_dependent_blocks(matrix __dm, int32_t __size,
                                                       : __dm[i][j];
 }
 
-// Much more clever kernel for double depenent blocks
+// Kernel for double depenent blocks (threads on whole line)
+__global__ void kernel_l_double_dependent_blocks(matrix __dm, int32_t __size,
+                                                 int32_t __b) {
+  int32_t k, j;
+
+  // Get some basic variables
+  const int32_t ib = blockIdx.x;
+  const int32_t jb = blockIdx.y;
+
+  // Real position in the grid
+  const int32_t ri = (ib << LOG_TILE_SIZE) + threadIdx.x;
+  const int32_t rj = (jb << LOG_TILE_SIZE);
+
+  // skip already calculated tile
+  if (ib == __b || jb == __b) return;
+
+  // check boundaries for matrices with indivisible size
+  if (ri >= __size) return;
+
+  for (k = __b * TILE_SIZE; k < (__b + 1) * TILE_SIZE; k++) {
+    if (k >= __size) break;
+    for (j = 0; j < TILE_SIZE; j++) {
+      if (rj + j >= __size) break;
+      // calculate values
+      __dm[ri][rj + j] = (__dm[ri][k] + __dm[k][rj + j] < __dm[ri][rj + j])
+                             ? __dm[ri][k] + __dm[k][rj + j]
+                             : __dm[ri][rj + j];
+    }
+  }
+}
+
+
+// Kernel for double depenent blocks (shared current block)
 __global__ void kernel_sm_double_dependent_blocks(matrix __dm, int32_t __size,
                                                   int32_t __b) {
-  // TODO
+  int32_t k;
+
+  // Get some basic variables
+  const int32_t ib = blockIdx.x;
+  const int32_t jb = blockIdx.y;
+
+  // Real position in the grid
+  const int32_t ri = threadIdx.x + (jb << LOG_TILE_SIZE);
+  const int32_t rj = threadIdx.y + (ib << LOG_TILE_SIZE);
+
+  // Position in shared memory grid
+  const int32_t i = threadIdx.x;
+  const int32_t j = threadIdx.y;
+
+  // skip already calculated tile
+  if (ib == __b || jb == __b) return;
+
+  // check boundaries for matrices with indivisible size
+  if (ri >= __size || rj >= __size) return;
+
+  __shared__ int32_t block[TILE_SIZE][TILE_SIZE];
+
+  // Copy data into shared memory
+  block[i][j] = __dm[ri][rj];
+  __syncthreads();
+
+  for (k = __b * TILE_SIZE; k < (__b + 1) * TILE_SIZE; k++) {
+    if (k >= __size) break;
+    // calculate values
+    block[i][j] = (__dm[ri][k] + __dm[k][rj] < block[i][j])
+                      ? __dm[ri][k] + __dm[k][rj]
+                      : block[i][j];
+  }
+
+  // Copy data back from shared into global memory
+  __dm[ri][rj] = block[i][j];
+  __syncthreads();
+}
+
+// Kernel for double depenent blocks (threads on whole line with shared memory block)
+__global__ void kernel_sml_double_dependent_blocks(matrix __dm, int32_t __size,
+                                                   int32_t __b) {
+  int32_t k, j;
+
+  // Get some basic variables
+  const int32_t ib = blockIdx.x;
+  const int32_t jb = blockIdx.y;
+
+  // Real position in the grid
+  const int32_t ri = (ib << LOG_TILE_SIZE) + threadIdx.x;
+  const int32_t rj = (jb << LOG_TILE_SIZE);
+
+  // Position in shared memory grid
+  const int32_t i = threadIdx.x;
+
+  // skip already calculated tile
+  if (ib == __b || jb == __b) return;
+
+  // check boundaries for matrices with indivisible size
+  if (ri >= __size) return;
+
+  __shared__ int32_t block[TILE_SIZE][TILE_SIZE];
+
+  // Copy data into shared memory
+  for (j = 0; j < TILE_SIZE; j++) {
+    if (rj + j >= __size) break;
+    block[i][j] = __dm[ri][rj + j];
+  }
+  __syncthreads();
+
+  for (k = __b * TILE_SIZE; k < (__b + 1) * TILE_SIZE; k++) {
+    __syncthreads();
+    if (k >= __size) continue;
+    for (j = 0; j < TILE_SIZE; j++) {
+      if (rj + j >= __size) break;
+
+      block[i][j] = (__dm[ri][k] + __dm[k][rj + j] < block[i][j])
+                        ? __dm[ri][k] + __dm[k][rj + j]
+                        : block[i][j];
+    }
+  }
+
+  // Copy data back from shared into global memory
+  for (j = 0; j < TILE_SIZE; j++) {
+    if (rj + j >= __size) break;
+    __dm[ri][rj + j] = block[i][j];
+  }
+  __syncthreads();
 }
 
 void run_algorithm(matrix __dm, int32_t __size) {
@@ -122,22 +242,39 @@ void run_algorithm(matrix __dm, int32_t __size) {
     // j-aligned singly depenent blocks
     for (k = b * s; k < (b + 1) * s; k++) {
       if (k >= __size) break;
-      kernel_column_dependent_blocks<<<tile_count, tile2D>>>(__dm, __size, b,
-                                                             k);
+      kernel_column_dependent_blocks<<<tile_count, tile2D>>>(__dm, __size, b, k);
       HANDLE_ERROR(cudaDeviceSynchronize());
     }
 
-// double dependent blocks
-#ifndef _SHARED
-    for (k = b * s; k < (b + 1) * s; k++) {
-      if (k >= __size) break;
-      kernel_double_dependent_blocks<<<grid2D, tile2D>>>(__dm, __size, b, k);
-      HANDLE_ERROR(cudaDeviceSynchronize());
+    // double dependent blocks
+    switch (_KERNEL) {
+      case 0:
+        for (k = b * s; k < (b + 1) * s; k++) {
+          if (k >= __size) break;
+          kernel_double_dependent_blocks<<<grid2D, tile2D>>>(__dm, __size, b, k);
+          HANDLE_ERROR(cudaDeviceSynchronize());
+        }
+        break;
+
+      case 1:
+        kernel_l_double_dependent_blocks<<<grid2D, tile2D>>>(__dm, __size, b);
+        HANDLE_ERROR(cudaDeviceSynchronize());
+        break;
+
+      case 2:
+        kernel_sm_double_dependent_blocks<<<grid2D, tile2D>>>(__dm, __size, b);
+        HANDLE_ERROR(cudaDeviceSynchronize());
+        break;
+
+      case 3:
+        kernel_sml_double_dependent_blocks<<<grid2D, tile2D>>>(__dm, __size, b);
+        HANDLE_ERROR(cudaDeviceSynchronize());
+        break;
+
+      default:
+        printf("Wrong Kernel specified.\n");
+        exit(EXIT_FAILURE);
     }
-#else
-    kernel_sm_double_dependent_blocks<<<grid2D, tile2D>>>(__dm, __size, b);
-    HANDLE_ERROR(cudaDeviceSynchronize());
-#endif
   }
 }
 
@@ -145,7 +282,7 @@ int main(int argc, char* argv[]) {
   int32_t size;
 
   FILE* graph_file;
-  matrix graph_mtx, dist_mtx2;
+  matrix graph_mtx;
   matrix hostMtx, devMtx;
 
   if (argc != 2) {
@@ -183,7 +320,7 @@ int main(int argc, char* argv[]) {
 
   printf("Checking that parallel algorithm runs correctly\n");
 
-  dist_mtx2 = get_distance_matrix(graph_mtx, size);
+  matrix dist_mtx2 = get_distance_matrix(graph_mtx, size);
   dist_mtx2 = floyd_warshall_seq(dist_mtx2, size);
   if (compare_matrices(hostMtx, dist_mtx2, size)) {
     printf("Both parallel and serial result matrices match.\n");
@@ -195,7 +332,11 @@ int main(int argc, char* argv[]) {
 
 #endif
 
+#ifdef _PRINT_RESULT
+
   print_matrix(hostMtx, size);
+
+#endif
 
   free_matrix_GPU(devMtx, size);
 
