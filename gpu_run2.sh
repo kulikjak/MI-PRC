98d812f96b7bb1962c87c61dd9d6d6d5650c8eb2
Jakub Kulík <kulikjak@gmail.com>
2017-12-20 16:47:50 +0100

    Added GPU runner for Dijkstra algo

---
+++ gpu_run2.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+echo "100"
+echo "100" >> cuda_dijkstra.out
+./cuda_dijkstra graph100.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph100.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph100.txt >> cuda_dijkstra.out
+
+echo "200"
+echo "200" >> cuda_dijkstra.out
+./cuda_dijkstra graph200.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph200.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph200.txt >> cuda_dijkstra.out
+
+echo "300"
+echo "300" >> cuda_dijkstra.out
+./cuda_dijkstra graph300.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph300.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph300.txt >> cuda_dijkstra.out
+
+echo "400"
+echo "400" >> cuda_dijkstra.out
+./cuda_dijkstra graph400.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph400.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph400.txt >> cuda_dijkstra.out
+
+echo "500"
+echo "500" >> cuda_dijkstra.out
+./cuda_dijkstra graph500.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph500.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph500.txt >> cuda_dijkstra.out
+
+echo "600"
+echo "600" >> cuda_dijkstra.out
+./cuda_dijkstra graph600.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph600.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph600.txt >> cuda_dijkstra.out
+
+echo "700"
+echo "700" >> cuda_dijkstra.out
+./cuda_dijkstra graph700.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph700.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph700.txt >> cuda_dijkstra.out
+
+echo "800"
+echo "800" >> cuda_dijkstra.out
+./cuda_dijkstra graph800.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph800.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph800.txt >> cuda_dijkstra.out
+
+echo "900"
+echo "900" >> cuda_dijkstra.out
+./cuda_dijkstra graph900.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph900.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph900.txt >> cuda_dijkstra.out
+
+echo "1000"
+echo "1000" >> cuda_dijkstra.out
+./cuda_dijkstra graph1000.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph1000.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph1000.txt >> cuda_dijkstra.out
+
+echo "1500"
+echo "1500" >> cuda_dijkstra.out
+./cuda_dijkstra graph1500.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph1500.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph1500.txt >> cuda_dijkstra.out
+
+echo "2000"
+echo "2000" >> cuda_dijkstra.out
+./cuda_dijkstra graph2000.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph2000.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph2000.txt >> cuda_dijkstra.out
+
+echo "2500"
+echo "2500" >> cuda_dijkstra.out
+./cuda_dijkstra graph2500.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph2500.txt >> cuda_dijkstra.out
+./cuda_dijkstra graph2500.txt >> cuda_dijkstra.out
